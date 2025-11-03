13f61d612098bb60a7b675386c2431e4da0516dc
abonneau <abonneau@vbox.42lyon.fr>
2025-11-03 16:55:56 +0100

    wordpress

---
+++ srcs/requirements/mariadb/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Initialize data directory if empty
+if [ ! -d /var/lib/mysql/mysql ]; then
+    echo "Initializing MariaDB data directory..."
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+fi
+
+# Start MariaDB as 'mysql' user, replace shell process
+exec gosu mysql mariadbd --datadir=/var/lib/mysql
